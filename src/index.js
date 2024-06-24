b3e8b4b4a16f1f5e51693cae87bf4c166a6a1d90
sksorth <144311554+sksorth@users.noreply.github.com>
2024-06-24 14:39:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.bsgonly.top": dockerHub,
+  "quay.bsgonly.top": "https://quay.io",
+  "gcr.bsgonly.top": "https://gcr.io",
+  "k8s-gcr.bsgonly.top": "https://k8s.gcr.io",
+  "k8s.bsgonly.top": "https://registry.k8s.io",
+  "ghcr.bsgonly.top": "https://ghcr.io",
+  "cloudsmith.bsgonly.top": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
